c32bd8c0d687d95997a6fe74395bc1c7bbd332a6
JaberSaed <jaber@capella.io>
2023-08-08 16:47:57 +0300

    update

--- app/app.ts
+++ app/app.ts
@@ -48,3 +48,16 @@ const logMessage = (message: string) => console.log(message);
 function logError(err: string): void {
   console.error(err);
 }
+
+
+let myResult: Result = {
+  playerNme: 'Marie',
+  score: 5,
+  problemCount: 5,
+  factor: 7
+};
+
+let player: Person ={
+  name:'Daniel',
+  formatName: () =>  'Daniel'
+}
