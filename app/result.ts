fc8c84593c634c41373c6f4f7c04b11b4883997b
JaberSaed <jaber@capella.io>
2023-08-08 16:00:41 +0300

    first commit

---
+++ app/result.ts
@@ -0,0 +1,6 @@
+interface Result {
+    playerNme: string;
+    score: number;
+    problemCount: number;
+    factor: number;
+}
---
+++ app/test.ts
@@ -0,0 +1,13 @@
+interface Employee {
+    name: string;
+    title: string;
+}
+
+let developer ={
+    name: 'Michelle',
+    title: 'Senior TpeScript Developer',
+    editor: 'Visual Studio Code'
+}
+
+let newEmployee: Employee ;
+newEmployee = developer;
